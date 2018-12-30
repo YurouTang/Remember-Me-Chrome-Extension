b5aea3991954e9ad0e1fc9461c683c67b53f6d42
Tang Yurou <tangyurou@gmail.com>
2018-12-30 14:15:41 +0800

    Basic Function - Add URL - Clear list of URLs

---
+++ background.js
@@ -0,0 +1,6 @@
+// Need to change
+// chrome.runtime.onInstalled.addListener(function() {
+//   chrome.storage.sync.set({websites: []}, function() {
+//     console.log("The color is green.");
+//   });
+// });
---
+++ popup.js
@@ -0,0 +1,47 @@
+$(function() {
+
+  chrome.storage.local.get('websites', function (result) {
+      var array = result.websites;
+      array.forEach(function(item) {
+        $("#web").append("<li>" + item + "</li>");
+      });
+  });
+
+  // $('#add').click(function() {
+  //   console.log("hello");
+  //   var url = $('#new_link').val();
+  //   chrome.storage.sync.set({'website': url});
+  //   $('#web').text(url);
+  //   chrome.storage.sync.get('website', function(website) {
+  //     console.log(website.website);
+  //   })
+  // });
+
+  $('#add').click(function() {
+
+    chrome.storage.local.get({websites: []}, function (result) {
+        var url = $('#new_link').val();
+        // the input argument is ALWAYS an object containing the queried keys
+        // so we select the key we need
+        var websiteList = result.websites;
+        websiteList.push(url);
+        // set the new array value to the same key
+        chrome.storage.local.set({websites: websiteList}, function () {
+            // you can use strings instead of objects
+            // if you don't  want to define default values
+            chrome.storage.local.get('websites', function (result) {
+                console.log(result.websites);
+            });
+        });
+        $("#web").append("<li>" + url + "</li>");
+        $('#new_link').val('');
+    });
+  })
+
+  $('#clear').click(function() {
+    console.log("cleared");
+    chrome.storage.local.set({websites: []}, function() {
+      $("#web").text("");
+    });
+  })
+});
